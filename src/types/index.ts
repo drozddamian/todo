543d85682efc657b1b153ec69a23f18e35b21607
Damian Drozd <damiandrozd.dev@gmail.com>
2020-09-16 08:56:27 +0200

    + Refactor VisibilityFilters component to use typescript

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,9 @@ export interface ITodo {
   content: string;
   completed: boolean;
 }
+
+export type visibilityFilterType = 'all' | 'completed' | 'incomplete'
+
+export interface VisibilityFiltersDictionary {
+  [key: string]: visibilityFilterType;
+}
